22c8f91a808576a0722594fba5c7370efd012933
Dominik Eichler <dspam@ldkf.de>
2017-01-29 20:14:34 +0100

    add plugin

--- js/functions.inc.js
+++ js/functions.inc.js
@@ -249,7 +249,7 @@ var dashboard = function() {
         
             $.post("backend/notepad.php", {action: "get", audioname: $('.activeaudio').text()}, function(data) {
         if(data==="$noentry$" || data==="<br>") {
-	   $('.notepad').html('<table style="width: 100%; border="1" id="t1"><tbody><tr><td style="width:5%">Zeit</td><td style="width:90%">Fehler</td><td style="width:5%">Status</td></tr><tr><td><br></td><td><br></td><td><br></td></tr></tbody></table><br><h5>Comments</h5><br>');
+	   $('.notepad').html('<table style="width: 100%; border="1" id="t1"><tbody><tr><td style="width:5%">Zeit</td><td style="width:85%">Fehler</td><td style="width:10%">Status</td></tr><tr><td><br></td><td><br></td><td><br></td></tr></tbody></table><br><h5>Comments</h5><br>');
         }
         else if(data==="0") {
             alert("Something went wrong.");
