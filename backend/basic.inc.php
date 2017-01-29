dec675c1f8e4a3b3f463a9a071f41735e1703d21
Dominik Eichler <dspam@ldkf.de>
2017-01-29 21:53:18 +0100

    add plugin

--- backend/basic.inc.php
+++ backend/basic.inc.php
@@ -3,7 +3,7 @@
 
 function getmonth($timestamp) {
     if(date("j", strtotime($timestamp))>15) {
-        return date("F Y", strtotime($timestamp . " +1 month"));
+        return date("F Y", strtotime($timestamp . " +18 day"));
     }
     else {
         return date("F Y", strtotime($timestamp));
