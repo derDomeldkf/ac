e6f08f95e47972fda443c1c6cf27d589fef7d951
Dominik Eichler <dspam@ldkf.de>
2017-01-30 01:43:58 +0100

    add plugin

--- js/functions.inc.js
+++ js/functions.inc.js
@@ -174,7 +174,7 @@ $(document).ready(function(){
 	 var size=500; 
 	 var audiocheck = $('.mistake_name').text();
 	 $('html, body').animate({scrollTop:0}, 500);
-	 $('.audioname_stats').fadeOut(size).delay( size ).html('');
+	 $('.audioname_stats').fadeOut(size).delay( size );
 	 $(".audioname_stats").css("margin", "0");
 	 $('.monthavg').delay( size ).fadeIn(1000);
 	 $('#overall').delay( size ).fadeIn(1000);
